f817fc58f9cd217f5483e83ba0a0ba4ea3aa0537
Edward Hobbs <edward24hobbs@outlook.com>
2015-12-04 14:37:30 +0000

    Code for day 4

---
+++ Day 4-2.py
@@ -0,0 +1,14 @@
+import hashlib
+
+key = 'bgvyzdsv'
+index = 0
+found = False
+while not found:
+    index += 1
+    hashable_key = hashlib.md5()
+    hashable_key.update(key+str(index))
+    hashed_key = hashable_key.hexdigest()
+    if hashed_key[0:6] == '000000':
+        found = True
+print index
+
